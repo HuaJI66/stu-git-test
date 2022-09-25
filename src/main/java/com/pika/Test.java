de7de18ecfc6183d0c853b1984d6de5052c576d3
pika <pika@pika.com>
2022-09-25 08:50:09 +0800

    version-0.0.1

---
+++ src/main/java/com/pika/Test.java
@@ -0,0 +1,16 @@
+package com.pika;
+
+/**
+ * Desc:
+ *
+ * @since: 2022/9/25 8:45
+ * @author: pikachu
+ */
+public class Test {
+    public Test() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
