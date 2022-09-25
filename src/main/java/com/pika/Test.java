c687e7fdd4caf119d6a875d93c27da2ef936c536
pika <pika@pika.com>
2022-09-25 10:30:49 +0800

    version-0.1.0h

--- src/main/java/com/pika/Test.java
+++ src/main/java/com/pika/Test.java
@@ -14,5 +14,6 @@ public class Test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("Git Push Test");
+        System.out.println("Git Push Test1");
     }
 }
